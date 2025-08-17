960e899345eba2fe6905c42af1c0af4ca768e6f5
Emilio-Pedro <pedroemiliocastro97@gmail.com>
2025-08-17 10:18:29 -0300

    ajuste 4 arquivos.kt

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.quiz.data.database.QuizDatabase
 import com.example.quiz.data.repository.QuestionRepository
@@ -14,9 +17,21 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        
-        // Inicializar questões no Firestore
+
+        /*ChatGPT - inicio
+            Quando o app inicia as barras de notificação e do sistema(em baixo)
+            continuam aparecendo e atrapalham o layout mesmo usando enableEdgeToEdge(),como remover elas?
+
+        */
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+        /*ChatGPT - final
+        */
+
         initializeQuestions()
         
         setContent {
